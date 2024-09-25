262275f28c9f3b711f4209362a0711fb1aa23889
Esau Jesse <jesseesau13@gmail.com>
2024-09-25 10:17:54 +0100

    Create Events model

--- event_management_api/events/models.py
+++ event_management_api/events/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Events(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    date_and_time = models.DateTimeField()
+    location = models.CharField(max_length=250)
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+    capacity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
