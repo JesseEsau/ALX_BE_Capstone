ca0efb2537f1c48259c8d59528b528bd672bc639
Esau Jesse <jesseesau13@gmail.com>
2024-09-27 20:53:07 +0100

    Create EventViewSet

--- event_management_api/events/views.py
+++ event_management_api/events/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from .models import Event
+from .serializers import EventSerializer
+
+class EventViewSet(viewsets.ViewSet):
+    """List and Retrieve Events."""
+
+    def list(self, request):
+        queryset = Event.objects.all()
+        serializer = EventSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Event.objects.all()
+        event = get_object_or_404(queryset, pk=pk)
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+
+
