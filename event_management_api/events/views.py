7f484ac3f05cf2fa916eb178a5edce3c0dfcf18b
Esau Jesse <jesseesau13@gmail.com>
2024-09-28 17:47:12 +0100

    Update EventListAPIView

--- event_management_api/events/views.py
+++ event_management_api/events/views.py
@@ -1,19 +1,22 @@
-from django.shortcuts import get_object_or_404
-
-from rest_framework import viewsets
-from rest_framework.response import Response
+from django.utils import timezone
 from rest_framework import generics
 
 from .models import Event
 from .serializers import EventSerializer
 
+#Create Events
 class EventCreateAPIView(generics.CreateAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
+#List all future Events
 class EventListAPIView(generics.ListAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    
+    #Show only future events
+    def get_queryset(self):
+        return Event.objects.filter(date_and_time__gte=timezone.now())
     
 
 
