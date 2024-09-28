8643984fec34871062a4907ae29292abd29cfa9c
Esau Jesse <jesseesau13@gmail.com>
2024-09-28 07:41:24 +0100

    removed viewset and added EventCreateAPIView

--- event_management_api/events/views.py
+++ event_management_api/events/views.py
@@ -2,13 +2,11 @@ from django.shortcuts import get_object_or_404
 
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework import generics
 
 from .models import Event
 from .serializers import EventSerializer
 
-class EventViewSet(viewsets.ViewSet):
-    """List and Retrieve Events."""
-
+class EventCreateAPIView(generics.CreateAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-
