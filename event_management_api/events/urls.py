4bd72a0e5ade0d43c13d457d805a07c368c3bc02
Esau Jesse <jesseesau13@gmail.com>
2024-09-28 07:54:26 +0100

    Remove unused import

--- event_management_api/events/urls.py
+++ event_management_api/events/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import EventCreateAPIView
-from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('events/create/', EventCreateAPIView.as_view(), name="create_event"),
