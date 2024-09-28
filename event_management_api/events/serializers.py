8005614f4a76076ce6a7099605fee357e793654a
Esau Jesse <jesseesau13@gmail.com>
2024-09-28 17:18:00 +0100

    Update Eventserializer

--- event_management_api/events/serializers.py
+++ event_management_api/events/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
+from rest_framework import status
+from rest_framework.response import Response
+from django.utils import timezone
 from .models import Event
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
+
+    def validate_date_and_time(self, value):
+            if value < timezone.now():
+                raise serializers.ValidationError({"error":"Event date cannot be in the past."})
+            return value
+
+
 
