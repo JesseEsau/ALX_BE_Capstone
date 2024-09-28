5e8eba260ad65ff897b97ff93639835ed4b1fddd
Esau Jesse <jesseesau13@gmail.com>
2024-09-28 20:33:41 +0100

    Update events/urls

--- event_management_api/events/urls.py
+++ event_management_api/events/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import EventCreateAPIView, EventListAPIView
+from .views import EventCreateAPIView, EventListAPIView, EventDetailView
 
 urlpatterns = [
     path('events/create/', EventCreateAPIView.as_view(), name="create_event"),
     path('events/', EventListAPIView.as_view(), name="events_list"),
+    path('events/<int:pk>/', EventDetailView.as_view(), name="event_detail"),
 ]
