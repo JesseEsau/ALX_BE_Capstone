9fa7f4797385d6990abe26822e65beb31eeacc99
Esau Jesse <jesseesau13@gmail.com>
2024-09-29 10:19:16 +0100

    Create UserRegistration view

--- event_management_api/accounts/views.py
+++ event_management_api/accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth.models import User
 
-# Create your views here.
+from .serializers import UserRegistrationSerializer
+
+class UserRegisterionAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
