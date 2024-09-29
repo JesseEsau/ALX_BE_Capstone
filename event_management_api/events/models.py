741982d694b247ccbf474beeca055873645ec3fe
Esau Jesse <jesseesau13@gmail.com>
2024-09-29 20:55:29 +0100

    update Event model

--- event_management_api/events/models.py
+++ event_management_api/events/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 
 class Event(models.Model):
     title = models.CharField(max_length=250)
-    description = models.TextField()
+    description = models.TextField(null=True)
     date_and_time = models.DateTimeField()
     location = models.CharField(max_length=250)
     organizer = models.ForeignKey(User, on_delete=models.CASCADE)
-    capacity = models.IntegerField()
+    capacity = models.IntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
