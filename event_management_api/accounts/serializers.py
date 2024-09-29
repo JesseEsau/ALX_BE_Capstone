f6809c46cb103cd88d0a89a1ffe983869a7b95f4
Esau Jesse <jesseesau13@gmail.com>
2024-09-29 10:34:12 +0100

    Update User Registration Serializer

--- event_management_api/accounts/serializers.py
+++ event_management_api/accounts/serializers.py
@@ -1,17 +1,17 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from rest_framework import status
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
+        model = User
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
         if User.objects.filter(email=validated_data['email']).exists():
-            raise serializers.ValidationError({"email": "This email is already in use."}, status=status.HTTP_400_BAD_REQUEST)
+            raise serializers.ValidationError({"email": "A user with this email already exist."})
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
