54609428995b3c6d6026ee1f907b4ae673df905e
Esau Jesse <jesseesau13@gmail.com>
2024-09-29 08:27:38 +0100

    Add simplejwt authentication class to settings

--- event_management_api/event_management_api/settings.py
+++ event_management_api/event_management_api/settings.py
@@ -42,6 +42,16 @@ INSTALLED_APPS = [
     'accounts',
 ]
 
+#jwt authentication
+REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+    
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
