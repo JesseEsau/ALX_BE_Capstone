642ce22da72d5ae23ee91eef1f17439203b68198
Esau Jesse <jesseesau13@gmail.com>
2024-09-30 14:02:25 +0100

    Update EventSerializer

--- event_management_api/events/serializers.py
+++ event_management_api/events/serializers.py
@@ -10,13 +10,6 @@ class EventSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ['organizer']
 
-    title = serializers.CharField(required=True)
-    date_and_time = serializers.DateTimeField(required=True)
-    location = serializers.CharField(required=True)
-
-    description = serializers.CharField(required=False)
-    capacity = serializers.IntegerField(required=False)
-
     def validate_date_and_time(self, value):
             if value < timezone.now():
                 raise serializers.ValidationError({"error":"Event date cannot be in the past."})
