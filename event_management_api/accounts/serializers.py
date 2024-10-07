5b9d1ed7b51ea2b5aff98029546d89e110d05282
Esau Jesse <jesseesau13@gmail.com>
2024-10-07 16:08:04 +0100

    Update accounts/serializers

--- event_management_api/accounts/serializers.py
+++ event_management_api/accounts/serializers.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
@@ -12,6 +14,8 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         if User.objects.filter(email=validated_data['email']).exists():
             raise serializers.ValidationError({"email": "A user with this email already exist."})
+        if len(validated_data['password']) < 5:
+            raise serializers.ValidationError({"detail": "Password must be at least 5 characters."}) 
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
