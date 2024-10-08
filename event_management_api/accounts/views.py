d6b210e1c089a7a3fed18f3317ebaca194f5971f
Esau Jesse <jesseesau13@gmail.com>
2024-10-08 14:48:34 +0100

    Update User Registration view

--- event_management_api/accounts/views.py
+++ event_management_api/accounts/views.py
@@ -4,5 +4,6 @@ from django.contrib.auth.models import User
 from .serializers import UserRegistrationSerializer
 
 class UserRegistrationAPIView(generics.CreateAPIView):
+    """Takes user credentials and stores them in the database."""
     queryset = User.objects.all()
     serializer_class = UserRegistrationSerializer
