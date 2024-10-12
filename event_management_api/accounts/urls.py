2a67c048e69e860862ed512ed38498e373458da4
Esau Jesse <jesseesau13@gmail.com>
2024-10-12 22:00:04 +0100

    Add logim and logout functionality for session auth.

--- event_management_api/accounts/urls.py
+++ event_management_api/accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import UserRegistrationAPIView
+from django.contrib.auth.views import LoginView
+from .views import LogoutView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -9,4 +11,6 @@ urlpatterns = [
     path('register/', UserRegistrationAPIView.as_view(), name='register'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path('logout/', LogoutView, name="logout"),
 ]
--- event_management_api/accounts/views.py
+++ event_management_api/accounts/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
 from django.contrib.auth.models import User
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
 from .serializers import UserRegistrationSerializer
 
@@ -7,3 +9,8 @@ class UserRegistrationAPIView(generics.CreateAPIView):
     """Takes user credentials and stores them in the database."""
     queryset = User.objects.all()
     serializer_class = UserRegistrationSerializer
+
+#log out view
+def LogoutView(request):
+    logout(request)
+    return redirect("/")
